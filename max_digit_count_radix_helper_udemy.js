8131412ee4178a75c57eb93d1ad381d49a66369d
Crew Spence <csspence123@gmail.com>
2020-08-12 05:08:06 -0500

    Created function stub for toy problem.

---
+++ max_digit_count_radix_helper_udemy.js
@@ -0,0 +1,16 @@
+/*
+JavaScript Algorithms and Data Structures Masterclass
+digitCount and maxDigitCount
+
+digitCount(num) returns the number of digits in num.
+
+maxDigitCount(nums) given an array of number returns the number of digits in the largest number
+*/
+
+const digitCount = (num) => {
+  return num.toString().length;
+}
+
+const maxDigitCount = (num) => {
+
+}
